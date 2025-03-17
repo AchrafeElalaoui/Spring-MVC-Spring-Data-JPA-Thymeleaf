57d48e5ce745f1589272126e594486e772cc4d0d
achraf <achraf.tata2004@gmail.com>
2025-03-17 14:04:24 +0000

    add entity ,repository and test

--- src/main/java/org/example/demo/DemoApplication.java
+++ src/main/java/org/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package org.example.demo;
 
+import org.example.demo.entities.Patient;
+import org.example.demo.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoApplication {
+import java.util.Date;
 
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        //add patients
+        patientRepository.save(new Patient(null, "Hassan",new Date(), false, 0));
+        patientRepository.save(new Patient(null, "mohamed", new Date(), true, 29));
+        patientRepository.save(new Patient(null, "khalid",new Date() , true, 19));
+
+
+    }
 }
---
+++ src/main/java/org/example/demo/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package org.example.demo.repository;
+
+import org.example.demo.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
