a85d63422c52fd40c756b37a562bdd0c3543f58d
achraf <achraf.tata2004@gmail.com>
2025-03-17 15:25:32 +0000

    Pagination

--- src/main/java/org/example/demo/web/PatientController.java
+++ src/main/java/org/example/demo/web/PatientController.java
@@ -5,9 +5,12 @@ import lombok.AllArgsConstructor;
 import org.example.demo.entities.Patient;
 import org.example.demo.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,9 +20,13 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("listPatients", patients);
-        return "patients";
+    public String index(Model model, @RequestParam(name="page",defaultValue = "0") int page , @RequestParam(name="size" ,defaultValue = "4") int size) {
+
+            Page<Patient> pagepatients = patientRepository.findAll(PageRequest.of(page, size));
+
+            model.addAttribute("listPatients", pagepatients.getContent());
+            model.addAttribute("pages", new int[pagepatients.getTotalPages()]);
+            model.addAttribute("currentPage", page);
+            return "patients";
+        }
     }
-}
